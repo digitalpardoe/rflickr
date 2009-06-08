46f8e35676a9897f7b3d7ebf999efed63e3d0942
Alex Pardoe <digital.pardoe@gmail.com>
2009-06-08 10:37:21 +0100

    More methods for convenience (not tested).

--- lib/api/ext/auth.rb
+++ lib/api/ext/auth.rb
@@ -3,10 +3,21 @@ require 'api/base/auth'
 require 'core/interaction/request'
 
 class AuthExt < Auth
+	attr_accessor :frob
+
 	def login_link(perms='read')
-		args = {'api_key' => @tokens.api_key, 'perms' => perms, 'frob' => get_frob['frob']['_content']}
+		@frob = get_frob['frob']['_content']
+		args = {'api_key' => @tokens.api_key, 'perms' => perms, 'frob' => @frob}
 		args = @api.sign_request(args)
 
 		Request.build_url('http://flickr.com/services/auth/', args)
+	end
+
+	def get_token(frob=@frob)
+		if frob
+			super(frob)
+		elsif
+			raise "You need call run login_link first if you want to call this method without arguments"
+		end
 	end
 end
