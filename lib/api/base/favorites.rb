b9214cdabf398c616ad23d46b7f851e371dd0983
Alex Pardoe <digital.pardoe@gmail.com>
2009-06-11 11:40:06 +0100

    The checks weren't really needed here.

--- lib/api/base/favorites.rb
+++ lib/api/base/favorites.rb
@@ -14,16 +14,7 @@ class Favorites < ApiBase
 		method = 'flickr.favorites.getList'
 		auth = true
 		get = true
-		args = {'user_id' => user_id, 'min_fave_date' => min_fave_date, 'max_fave_date' => max_fave_date, 'per_page' => per_page, 'page' => page}
-
-		extras_accepted_values = [ 'license', 'date_upload', 'date_taken', 'owner_name', 'icon_server', 'original_format', 'last_update', 'geo', 'tags', 'machine_tags', 'o_dims', 'views', 'media' ]
-		valid_extras = extras ? extras_accepted_values.include?(extras) : true
-
-		if valid_extras == false
-			raise ArgumentError, "invalid extras value, must be one of - #{extras_accepted_values.join(', ')}"
-		else
-			args = args.merge({'extras' => extras})
-		end
+		args = {'user_id' => user_id, 'min_fave_date' => min_fave_date, 'max_fave_date' => max_fave_date, 'extras' => extras, 'per_page' => per_page, 'page' => page}
 
 		@api.call(method, args, auth, nil, get)
 	end
@@ -32,16 +23,7 @@ class Favorites < ApiBase
 		method = 'flickr.favories.getPublicList'
 		auth = false
 		get = true
-		args = {'user_id' => user_id, 'min_fave_date' => min_fave_date, 'max_fave_date' => max_fave_date, 'per_page' => per_page, 'page' => page}
-
-		extras_accepted_values = [ 'license', 'date_upload', 'date_taken', 'owner_name', 'icon_server', 'original_format', 'last_update', 'geo', 'tags', 'machine_tags', 'o_dims', 'views', 'media' ]
-		valid_extras = extras ? extras_accepted_values.include?(extras) : true
-
-		if valid_extras == false
-			raise ArgumentError, "invalid extras value, must be one of - #{extras_accepted_values.join(', ')}"
-		else
-			args = args.merge({'extras' => extras})
-		end
+		args = {'user_id' => user_id, 'min_fave_date' => min_fave_date, 'max_fave_date' => max_fave_date, 'extras' => extras, 'per_page' => per_page, 'page' => page}
 
 		@api.call(method, args, auth, nil, get)
 	end
