bbb8ea39984af145a667a81e157803abf01fb65c
Alex Pardoe <digital.pardoe@gmail.com>
2009-06-11 09:49:02 +0100

    Implemented 'contacts' methods.

--- lib/api/base/contacts.rb
+++ lib/api/base/contacts.rb
@@ -1,15 +1,48 @@
 require 'core/api_base'
 
 class Contacts < ApiBase
-	def get_list
+	def get_list(filter=nil, page=nil, per_page=nil)
+		method = 'flickr.contacts.getList'
+		auth = true
+		get = true
+		args = {'page' => page, 'per_page' => per_page}
 
+		filter_accepted_values = [ 'friends', 'family', 'both', 'neither' ]
+		valid_filter = filter ? filter_accepted_values.include?(filter) : nil
+
+		if valid_filter == false
+			raise ArgumentError, 'invalid filter value, must be one of - friends, family, both, neither'
+		else
+			args = args.merge({'filter' => valid_filter})
+		end
+
+		@api.call(method, args, auth, nil, get)
 	end
 
-	def get_list_recently_uploaded
+	def get_list_recently_uploaded(date_lastupload=nil, filter=nil)
+		method = 'flickr.contacts.getListRecentlyUploaded'
+		auth = true
+		get = true
+		args = {'date_lastupload' => date_lastupload}
 
+		filter_accepted_values = [ 'ff', 'all' ]
+		valid_filter = filter ? filter_accepted_values.include?(filter) : nil
+
+		if valid_filter == false
+			raise ArgumentError, 'invalid filter value, must be one of - ff, all'
+		else
+			args = args.merge({'filter' => valid_filter})
+		end
+
+		@api.call(method, args, auth, nil, get)
 	end
 
-	def get_public_list
+	def get_public_list(user_id, page=nil, per_page=nil)
+		method = 'flickr.contacts.getPublicList'
+		auth = false
+		get = true
+		args = {'user_id' => user_id, 'page' => page, 'per_page' => per_page}
 
+		@api.call(method, args, auth, nil, get)
 	end
 end
---
+++ spec/api/contacts_spec.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Contacts do
+	before(:each) do
+		@flickr = Flickr.new(@@api_data['api_key'], @@api_data['shared_secret'], { :auth_token => @@api_data['auth_token'], :extended => true })
+	end
+
+	it "should sucessfully get the list of contacts" do
+		@flickr.contacts.get_list['stat'].should == 'ok'
+	end
+
+	it "should sucessfully get the recently uploaded list" do
+		@flickr.contacts.get_list_recently_uploaded['stat'].should == 'ok'
+	end
+
+	it "should throw an argument error when getting the list" do
+		lambda{ @flickr.contacts.get_list('something', nil, nil) }.should raise_error(ArgumentError)
+	end
+
+	it "should throw an arugment error when getting the recently uploaded list" do
+		lambda{ @flickr.contacts.get_list_recently_uploaded(nil, 'something') }.should raise_error(ArgumentError)
+	end
+end
+
