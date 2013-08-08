06caac62c84d73a429f0b43e47a033900f3d8bec
Alex Pardoe <digital.pardoe@gmail.com>
2013-08-08 16:36:32 +0100

    Removed unneeded test.

--- spec/flickr_spec.rb
+++ spec/flickr_spec.rb
@@ -2,43 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Flickr do
 	before(:each) do
-		@flickr_auth = Flickr.new(Flickr.class_variable_get(:@@api_data)['api_key'], Flickr.class_variable_get(:@@api_data)['shared_secret'], :auth_token => Flickr.class_variable_get(:@@api_data)['auth_token'])
+		@flickr_auth = Flickr.new(Flickr.class_variable_get(:@@api_data)['api_key'], Flickr.class_variable_get(:@@api_data)['shared_secret'], Flickr.class_variable_get(:@@api_data)['auth_token'])
 		@flickr_norm = Flickr.new(Flickr.class_variable_get(:@@api_data)['api_key'], Flickr.class_variable_get(:@@api_data)['shared_secret'])
-	end
-
-	it "should sucesfully create all objects" do
-		@flickr_auth.activity.should_not == nil
-		@flickr_auth.auth.should_not == nil
-		@flickr_auth.blogs.should_not == nil
-		@flickr_auth.collections.should_not == nil
-		@flickr_auth.commons.should_not == nil
-		@flickr_auth.contacts.should_not == nil
-		@flickr_auth.favorites.should_not == nil
-		@flickr_auth.groups.should_not == nil
-		@flickr_auth.interestingness.should_not == nil
-		@flickr_auth.machinetags.should_not == nil
-		@flickr_auth.panda.should_not == nil
-		@flickr_auth.people.should_not == nil
-		@flickr_auth.photos.should_not == nil
-		@flickr_auth.photosets.should_not == nil
-		@flickr_auth.places.should_not == nil
-		@flickr_auth.prefs.should_not == nil
-		@flickr_auth.reflection.should_not == nil
-		@flickr_auth.test.should_not == nil
-		@flickr_auth.tags.should_not == nil
-		@flickr_auth.urls.should_not == nil
-	end
-
-	it "should sucesfully create all sub-objects" do
-		@flickr_auth.groups.members.should_not == nil
-		@flickr_auth.groups.pools.should_not == nil
-		@flickr_auth.photos.comments.should_not == nil
-		@flickr_auth.photos.geo.should_not == nil
-		@flickr_auth.photos.licenses.should_not == nil
-		@flickr_auth.photos.notes.should_not == nil
-		@flickr_auth.photos.transform.should_not == nil
-		@flickr_auth.photos.upload.should_not == nil
-		@flickr_auth.photosets.comments.should_not == nil
 	end
 
 	it "should connect and make api request" do
